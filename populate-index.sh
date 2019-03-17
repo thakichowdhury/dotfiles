0dbd3f4fbca80410321cfe76570024283eedc0d6
Thaki Chowdhury <thaki@flyblackbird.com>
2019-03-17 11:53:23 -0700

    Create bash script to read files in directories and create index file sourcing the config files

---
+++ populate-index.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+VIMDIR=./vim
+ZSHDIR=./zsh
+PWDLEN="${#PWD}"
+
+declare -a directories=(
+  "./vim"
+  "./zsh"
+  )
+
+length () {
+  return "${1}"
+}
+
+make_index () {
+  for f in $1/*
+  do
+    if [[ $f =~ "index" ]];
+    then
+      echo 'clearing existing index file'
+      > $f
+    else
+      file=$(echo $f | cut -c 3-)
+      file_absolute_path="$PWD/$file"
+      extension=$(echo $1 | cut -c 3-)
+      index_file="$1/0-index.$extension"
+      echo "sourcing from $file_absolute_path"
+      echo source $file_absolute_path >> $index_file
+    fi
+  done
+}
+
+for d in ${directories[@]}
+do
+  echo "creating index file at $d"
+  make_index $d
+done
+
+# number_of_directories=${#directories[@]}
+# for (( i=0; i<${number_of_directories}; i++ ));
+# do
+#   dir=${directories[$i]}
+#   echo "creating index file at $dir"
+#   make_index $dir
+# done
