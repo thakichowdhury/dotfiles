9a413f2b0b8e768900ef8bea7a12b65037fd4875
Thaki Chowdhury <thaki@flyblackbird.com>
2019-03-29 18:14:53 -0700

    Add function to zsh to fetch and checkout a remote branch

--- zsh/2-functions.zsh
+++ zsh/2-functions.zsh
@@ -1,7 +1,12 @@
 # formatting
-function format_branch_name() {
+format_branch_name() {
   ticket_name=${1//' '/'-'}
   formatted_branch=$2'/'$ticket_name:l
   echo $formatted_branch
   echo $formatted_branch | pbcopy
+  git branch -b $formatted_branch
+}
+
+checkout_remote_branch() {
+  git fetch && git checkout $1
 }
