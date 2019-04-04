5facff070bd260c30ac9c755883311c1afcce457
Thaki Chowdhury <thaki@flyblackbird.com>
2019-04-03 21:06:45 -0700

    Add functions to checkout remote git branch and clean an npm project directory

--- zsh/2-functions.zsh
+++ zsh/2-functions.zsh
@@ -7,6 +7,26 @@ format_branch_name() {
   git branch -b $formatted_branch
 }
 
-checkout_remote_branch() {
+# git
+
+## check out remote branch
+crb() {
+  echo "checking out $1"
   git fetch && git checkout $1
+}
+
+## remove node modules, npm and yarn lock files, and reinstalls
+clean_directory() {
+  echo "removing node modules...\n"
+  rm -rf node_modules
+  echo "checking for lock files\n"
+  if [ -f ./package-lock.json ]; then
+    echo "  removing package-lock.json...\n"
+    rm package-lock.json
+  elif [ -f ./yarn.lock ]; then
+    echo "  removing yarn.lock...\n"
+    rm yarn.lock
+  fi
+  echo "installing node modules...\n"
+  npm install
 }
