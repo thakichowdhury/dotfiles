ee8b823bef9aa447c7178b2f2bf91fdf11c6f317
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2019-11-13 00:38:37 -0800

    Add core zsh settings

--- zsh/1-core.zsh
+++ zsh/1-core.zsh
@@ -1,4 +1,19 @@
-# FZF
+# Themes
+ZSH_THEME="agnoster"
+
+
+# Path to your oh-my-zsh installation.
+export ZSH="/Users/thaki_britney/.oh-my-zsh"
+
+source $ZSH/oh-my-zsh.sh
+
+
+# sensitive completion must be off. _ and - will be interchangeable.
+ HYPHEN_INSENSITIVE="true"
+
+
+export DEFAULT_USER="$(whoami)"
+
 # fd --type f | fzf                                   # Feed the output of fd into fzf
 export FZF_DEFAULT_COMMAND='fd --type f'            # Setting fd as the default source for fzf
 # fzf                                                 # Now fzf (w/o pipe) will use fd instead of find
