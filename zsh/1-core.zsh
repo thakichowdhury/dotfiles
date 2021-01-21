44dae3d485e39692467ee6e36ed6a3548618fcde
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2021-01-21 10:29:17 -0800

    Add Android paths for React Native setup as well as new Java SDK path that broke when updating OSX to Big Sur

--- zsh/1-core.zsh
+++ zsh/1-core.zsh
@@ -18,3 +18,15 @@ export DEFAULT_USER="$(whoami)"
 export FZF_DEFAULT_COMMAND='fd --type f'            # Setting fd as the default source for fzf
 # fzf                                                 # Now fzf (w/o pipe) will use fd instead of find
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"    # To apply the command to CTRL-T as well
+
+# Android paths for React Native setup
+export ANDROID_HOME=/Users/thaki_britney/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+# update java sdk path since an error occurs since update to OSX Big Sur
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+export PATH=$JAVA_HOME/bin:$PATH
+
