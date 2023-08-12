b255adc0a9c011f9c37f7e85cebaf754ed2ae74e
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2023-08-11 17:29:05 -0700

    lint: remove extraneous print statements

--- bin/setup/symlink-config-files.sh
+++ bin/setup/symlink-config-files.sh
@@ -5,13 +5,13 @@ symlink_config_files() {
   extension=$1
   # create current rc file path
   rc_file_path=~/."${extension}rc"
-  echo "extension: $extension, rc_file_path: $rc_file_path"
 
 #   # backup and remove the current rc file, if one exists
   create_backups $rc_file_path $extension
 
   # create a symlink for the new rc to replace the old rc
   dotfile_config_path="$ROOT_DIR/$extension/0-index.ignore.$extension"
+
   # symlink source_path -> target_path
   ln -s $dotfile_config_path $rc_file_path
   echo "Created symlink from $rc_file_path to $dotfile_config_path!"
